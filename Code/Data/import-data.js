624f231168aece7df54ee01631b0fa6396501db5
PSergiannis <psergiannis@hotmail.com>
2023-12-06 20:29:46 +0200

    New data insertion script

--- Code/Data/import-data.js
+++ Code/Data/import-data.js
@@ -16,48 +16,29 @@ const query = util.promisify(connection.query).bind(connection);
 
     // Insert Categories
     for (const category of data.categories) {
-      // Adjust the object keys to match the database column names
-      const categoryToInsert = {
-        id: category.id, // Assuming 'id' is correct
-        name: category.category_name, // Change 'category_name' to 'name'
-      };
-      const result = await query(
-        "INSERT INTO Category SET ?",
-        categoryToInsert
-      );
-      console.log("Category inserted", result.insertId);
+      const insertCategoryQuery =
+        "INSERT INTO Category (id, name) VALUES (?, ?)";
+      await query(insertCategoryQuery, [category.id, category.category_name]);
     }
 
-    // Insert Products and Details
+    // Insert Products and their Details
     for (const item of data.items) {
-      // Adjust item properties to match your Product table column names
-      const productToInsert = {
-        // Assuming item has properties that match the Product table columns
-        id: item.id,
-        name: item.name,
-        category: item.category,
-      };
-      const resultProduct = await query(
-        "INSERT INTO Product SET ?",
-        productToInsert
-      );
-      console.log("Product inserted", resultProduct.insertId);
+      const insertProductQuery =
+        "INSERT INTO Product (id, name, category) VALUES (?, ?, ?)";
+      await query(insertProductQuery, [item.id, item.name, item.category]);
 
-      // Now insert each detail for the current item
       for (const detail of item.details) {
-        // Map detail properties to the Details table column names
-        const detailToInsert = {
-          product: item.id, // Foreign key linking to the Product
-          name: detail.detail_name, // Adjust the key if necessary
-          value: detail.detail_value, // Adjust the key if necessary
-        };
-        const resultDetail = await query(
-          "INSERT INTO Details SET ?",
-          detailToInsert
-        );
-        console.log("Detail inserted", resultDetail.insertId);
+        const insertDetailQuery =
+          "INSERT INTO Details (product, name, value) VALUES (?, ?, ?)";
+        await query(insertDetailQuery, [
+          item.id,
+          detail.detail_name,
+          detail.detail_value,
+        ]);
       }
     }
+
+    console.log("Data imported successfully.");
   } catch (err) {
     console.error("An error occurred: " + err.message);
   } finally {
