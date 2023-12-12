2be59e53b70858dc19427867a3fde0e0f4f01204
Christina Kratιmenou <xriskt@gmail.com>
2023-12-12 21:01:52 +0200

    Update Login / Initial Inventory API

--- Code/Web App/js/api.js
+++ Code/Web App/js/api.js
@@ -1,20 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const config = require('config.js');
+const express = require("express");
+const mysql = require("mysql2");
+const dbconfig = require("C:/xampp/htdocs/e-ethelodria/config.json");
+const session = require("express-session");
+const path = require("path"); 
+//const bcrypt = require("bcrypt");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
-const db = mysql.createConnection(config.db);
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
+const db = mysql.createConnection({
+  host: dbconfig.db.host,
+  user: dbconfig.db.user,
+  password: dbconfig.db.password,
+  database: dbconfig.db.database
 });
 
+db.connect( (error) => {
+   if(error) {
+      console.log("Error connecting to MySQL", error);
+      process.exit(1); 
+   } else {
+      console.log("MySQL Connected...");
+   } 
+});
+
+/*
+// Configure Handlebars
+app.engine('html', exphbs.create({
+  extname: '.html', // Set the file extension to .html
+}));
+app.set('view engine', 'html');
+app.engine('html', hbs.engine);
+
+// Set the views directory to the folder where your HTML files are located
+app.set('views', path.join(__dirname, '/views'));
+*/
 app.use(session({
    secret: 'secret',
    resave: true,
@@ -28,25 +48,37 @@ app.get('/', function(request, response) {
    response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.post('/auth', function(request, response) {
+app.post('/login', function(request, response, next) {
 
-   let username = request.body.username;
-   let password = request.body.password;
+   var username = request.body.username;
+   var password = request.body.password;
+   //console.log(username);
+   //console.log(password);
+
    if (username && password) {
       // Execute SQL query that'll select the account from the database based on the specified username and password
-      connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-         // Error output
-         if (error) throw error;
+      query = 'SELECT * FROM user WHERE username = ? AND password = ?';
+      db.query(query, function(error, data) {
          // If the account exists
-         if (results.length > 0) {
-            request.session.loggedin = true;
-            request.session.username = username;
-            // Redirect to home page
-            response.redirect('/home');
+         if (data.length > 0) {
+
+            for (var count = 0; count < data.length; count++) {
+               
+               if(data[count].password == password) {
+                  request.session.username = data[count].username;
+                  response.redirect("/user_home.html");
+               }
+               else { 
+                  response.send('Incorrect Password');
+
+               }
+            }
+
          } else {
             response.send('Incorrect Username and/or Password!');
+            response.end();
          }        
-         response.end();
+     
       });
    } else {
       response.send('Please enter Username and Password!');
@@ -54,13 +86,12 @@ app.post('/auth', function(request, response) {
    }
 });
 
-app.get('/home', function(request, response) {
-   if (request.session.loggedin) {
-      response.send('Welcome back, ' + request.session.username + '!');
-   } else {
-      response.send('Please login to view this page!');
-   }
-   response.end();
+app.get('/logout', function(requst, response, next) {
+   request.session.destroy();
+
+   response.redirect("/user_home.html");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+   console.log("Server started on port 3000");
+});
