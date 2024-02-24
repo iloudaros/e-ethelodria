323ad5129356ac829de09e7def8f5afb20796d1f
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-02-24 17:40:01 +0200

    Changes before break

--- Code/Web App/js/map.js
+++ Code/Web App/js/map.js
@@ -1,34 +1,24 @@
 $(document).ready(function () {
-    var map = L.map('map').setView([39.3622, 22.9424], 13); // Default location: Volos, Greece
+    var map = L.map('map').setView([39.3622, 22.9424], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
 
-    L.control.locate({
-        strings: {
-          title: "Show me where I am, yo!"
-        }
-      })
-      .addTo(map);
+    // Add Locate Control
+    L.control.locate().addTo(map);
 
-    // Fetch markers' coordinates from the server
+    // Fetch markers from the server
     $.ajax({
         url: 'php/get_markers.php',
         method: 'GET',
-        dataType: 'json',
-        success: addMarkers(data),
-        error: console.error('Error fetching markers:', error);
-        });
+        success: function (data) {
+            var markers = JSON.parse(data);
 
-
-
-    function addMarkers(markers) {
-        markers.forEach(function (marker) {
-            L.marker([marker.lat, marker.lng]).addTo(map)
-                .bindPopup(marker.name);
-        });
-    }
-
-
+            markers.forEach(function (marker) {
+                L.marker([marker.lat, marker.lng]).addTo(map)
+                    .bindPopup(marker.name);
+            });
+        }
+    });
 });
--- Code/Web App/php/get_markers.php
+++ Code/Web App/php/get_markers.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'config.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -7,7 +8,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM Vehicle";
+$sql = "SELECT * FROM places_of_interest";
 $result = $conn->query($sql);
 
 $markers = array();
@@ -15,15 +16,14 @@ $markers = array();
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $markers[] = array(
-            'id' => $row['id'],
+            'name' => $row['name'],
             'lat' => $row['latitude'],
             'lng' => $row['longitude']
         );
     }
 }
 
-$conn->close();
-
-header('Content-Type: application/json');
 echo json_encode($markers);
+
+$conn->close();
 ?>
