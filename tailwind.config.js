0bcb38236511d4d80972f7a14322d72f31d4ae8a
PSergiannis <psergiannis@hotmail.com>
2024-03-04 17:25:00 +0200

    tailwind install & gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+    "./**/*.jsx",
+    "./**/*.ts",
+    "./**/*.tsx",
+    "./**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
