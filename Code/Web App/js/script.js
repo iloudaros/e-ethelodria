2c0a1c01f121cc6bc9ea484a9288dfbb5f0507be
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-08-24 18:21:33 +0300

    bombed

--- Code/Web App/js/script.js
+++
@@ -1,5 +0,0 @@
-// Add any JavaScript functionality if needed
-// For example, handling sign-up button click event
-document.getElementById('signup-button').addEventListener('click', function () {
-    alert('Sign up functionality coming soon!');
-});
